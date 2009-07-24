d0836c7afa9007bb44d11e8f4b632eb66f7bb5ba
Christoph Helma <helma@in-silico.de>
2009-07-24 12:40:54 +0200

    Initial import of lazar-webservice-gui

---
+++ lazar.rb
@@ -0,0 +1,100 @@
+['rubygems', 'sinatra', 'rest_client', 'sinatra/url_for', 'haml', 'sass', 'crack/xml'].each do |lib|
+	require lib
+end
+
+LAZAR_URI = 'http://webservices.in-silico.ch/lazar/models/'
+#LAZAR_URI = 'http://localhost:3000/models/'
+COMPOUNDS_URI = 'http://webservices.in-silico.ch/compounds/'
+
+get '/' do 
+	@models = []
+	(RestClient.get LAZAR_URI).chomp.each_line do |uri|
+		puts uri
+		xml = RestClient.get uri
+		@models << Crack::XML.parse(xml)['model']
+		name = Crack::XML.parse(xml)['model']['name']
+	end
+	haml :index
+end
+
+get '/create' do
+	haml :create
+end
+
+get '/neighbors' do
+	#@lazar  = params[:lazar]
+	#haml :neighbors
+	"Sorry, display of neighbors is not (yet) implemented."
+end
+
+get '/:id' do # input form for predictions
+	begin
+		xml = RestClient.get "#{LAZAR_URI}#{params[:id]}"
+		@model = Crack::XML.parse(xml)['model']
+	 	YAML.load(RestClient.get "#{@model['validation']['details_uri']}") # check if model creation is finished
+		haml :show
+ 	rescue
+		redirect "/tmp/#{params[:id]}"
+ 	end
+end
+
+get '/validation/:id' do 
+	begin
+	 xml = RestClient.get "#{LAZAR_URI}#{params[:id]}"
+	 @model = Crack::XML.parse(xml)['model']
+	 @summary = YAML.load(RestClient.get "#{@model['validation']['summary_uri']}")
+	 haml :validation
+ 	rescue
+		redirect "/tmp/#{params[:id]}"
+ 	end
+end
+
+get '/delete/:id' do
+	xml = RestClient.get "#{LAZAR_URI}#{params[:id]}"
+	@model = Crack::XML.parse(xml)['model']
+	haml :delete
+end
+
+get '/tmp/:id' do
+	haml :tmp
+end
+
+post '/' do # create a new model
+	# create dataset
+	# validate lazar on the dataset
+	sanitized_name = params[:name].gsub(/\s+/,'_').gsub(/[^[:alnum:]]/, '')
+	uri = `curl -F name=#{sanitized_name} -F file=@#{params[:file][:tempfile].path} -F username=#{params[:username]} -F password=#{params[:password]} #{LAZAR_URI}`
+	id = uri.chomp.gsub(/^.*\/(\d+)$/,'\1')
+	redirect "/#{id}"
+end
+
+post '/:id' do # post chemical name to model
+	begin
+		smiles = RestClient.get "#{COMPOUNDS_URI}#{URI.encode(params[:name])}.smiles"
+	rescue
+		@message = "Can not retrieve Smiles from #{params[:name]}. Please try again."
+		redirect "/#{params[:id]}"
+	end
+	begin
+		xml = RestClient.get "#{LAZAR_URI}#{params[:id]}?smiles=#{URI.encode(smiles)}"
+		@lazar = Crack::XML.parse(xml)['lazar']
+		haml :prediction
+	rescue
+		"Prediction for #{params[:name]} (SMILES #{smiles}) failed." 
+	end
+end
+
+delete '/:id' do
+	begin
+		RestClient.delete "#{LAZAR_URI}#{params[:id]}", :username => params[:username], :password => params[:password]
+		redirect '/'
+	rescue
+		"Deletion of model with ID #{params[:id]} failed. Please check your username and password."
+	end
+end
+
+# SASS stylesheet
+get '/stylesheets/style.css' do
+  headers 'Content-Type' => 'text/css; charset=utf-8'
+  sass :style
+end
