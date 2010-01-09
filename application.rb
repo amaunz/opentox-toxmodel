08103ef7fca40d0dd6c6813caea56eaf0e0af9ea
Christoph Helma <helma@in-silico.de>
2010-01-09 18:59:26 +0100

    first demonstration version for lazar

--- application.rb
+++ application.rb
@@ -1,33 +1,26 @@
 ['rubygems', 'opentox-ruby-api-wrapper', "haml", "sass"].each do |lib|
 	require lib
 end
+require 'rack-flash'
 
-get '/?' do 
-	@models = OpenTox::Model::Lazar.find_all
-	haml :index
+use Rack::Flash
+set :sessions, true
+
+get '/' do
+	redirect '/create'
+end
+
+get '/predict/?' do 
+	@models = OpenTox::Model::Lazar.find_all#.each { |uri| @models << OpenTox::Model::Generic.new(uri) }
+	haml :predict
 end
 
 get '/create' do
 	haml :create
 end
 
-get '/:id' do # input form for predictions
-	@model = OpenTox::Model::Lazar.find(File.join(@@config[:services]["opentox-model"],params[:id]))
-	haml :show
-end
-
-get '/validation/:id' do 
-	@model = OpenTox::Model::LazarClassification.find(params[:id])
-	haml :validation
-end
-
-get '/delete/:id' do
-	@model = OpenTox::Model::LazarClassification.find(params[:id])
-	haml :delete
-end
-
-get '/tmp/:id' do
-	haml :tmp
+get '/about' do
+	haml :about
 end
 
 post '/' do # create a new model
@@ -35,35 +28,55 @@ post '/' do # create a new model
 	training_data = OpenTox::Dataset.create(data)
 	@features = training_data.features
 	if @features.size == 1
-		feature_data = OpenTox::Algorithm::Fminer.create(:dataset_uri => training_data.uri, :feature_uri => @features[0].to_s)
-		@model = OpenTox::Model::Lazar.create(:activity_dataset_uri => training_data.uri, :feature_dataset_uri => feature_data.to_s)
+		OpenTox::Algorithm::Lazar.create_model(:dataset_uri => training_data.uri, :feature_uri => @features.first)
+		flash[:notice] = "Model creation started. If you reload this page the new model will appear in the selection list as soon as it is finished."
+		redirect '/predict'
 		haml :index
 	else
-		halt 400, "The dataset contains more than one target variable:\n#{@features.collect{|f| f.to_s}.join("\n")}\nPlease clean up and submit it again."
+		halt 400, "The dataset contains more than one target variable:\n#{@features.collect{|f| f.to_s}.join("\n")}\nPlease clean up and submit again."
 		redirect "/features"
 	end
 end
 
-post '/:id' do # post chemical name to model
+post '/predict/?' do # post chemical name to model
+	@identifier = params[:identifier]
+	begin
+		@compound = OpenTox::Compound.new(:name => params[:identifier])
+	rescue
+		flash[:notice] = "Could not find a structure for #{@identifier}. Please try again."
+		redirect '/predict'
+	end
+	@predictions = []
+	params[:selection].keys.each do |uri|
+		prediction = nil
+		confidence = nil
+		title = nil
+		prediction = RestClient.post uri, :compound_uri => @compound.uri, :accept => "application/x-yaml"
+		model = Redland::Model.new Redland::MemoryStore.new
+		parser = Redland::Parser.new
+		parser.parse_string_into_model(model,prediction,'/')
+		f = model.subject(RDF['type'],OT['Feature']) # this can be dangerous if OWL is not properly sorted
+		title = model.object(f,DC['title']).to_s
+		puts title
+		model.subjects(RDF['type'], OT['FeatureValue']).each do |v|
+			feature = model.object(v,OT['feature'])
+			feature_name = model.object(feature,DC['title']).to_s
+			prediction = model.object(v,OT['value']).to_s if feature_name.match(/classification/)
+			confidence = model.object(v,OT['value']).to_s if feature_name.match(/confidence/)
+		end
+		case prediction.to_s
+		when "true"
+			prediction = "active"
+		when "false"
+			prediction = "inactive"
+		else
+			prediction = "not available"
+		end
+		@predictions << {:title => title, :prediction => prediction, :confidence => confidence}
+	end
 
-	storage = Redland::MemoryStore.new
-	parser = Redland::Parser.new
-	serializer = Redland::Serializer.new
-	prediction = Redland::Model.new storage
-
-	#begin
-		@compound = OpenTox::Compound.new(:name => params[:name])
-		#@compound = OpenTox::Compound.new(:smiles => params[:name])
-	#rescue
-	#	@message = "Can not retrieve Smiles from #{params[:name]}. Please try again."
-	#	redirect "/#{params[:id]}"
-	#end
-	@lazar = OpenTox::Model::Lazar.find(File.join(@@config[:services]["opentox-model"],params[:id]))
-	parser.parse_string_into_model(prediction,@lazar.predict(@compound).to_s,'/')
-	@classification = prediction.object(Redland::Uri.new(@compound.uri),Redland::Uri.new(File.join(@@config[:services]["opentox-model"],params[:id],"classification")))
-	@confidence = prediction.object(Redland::Uri.new(@compound.uri),Redland::Uri.new(File.join(@@config[:services]["opentox-model"],params[:id],"confidence")))
-	@measured_activity = prediction.object(Redland::Uri.new(@compound.uri),Redland::Uri.new(@lazar.endpoint))
 	haml :prediction
+	#@predictions.to_yaml 
 end
 
 delete '/:id' do
