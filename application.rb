53af6c563627bdb69eeceefecf2dc9ca62bd052e
Christoph Helma <helma@in-silico.de>
2010-02-18 15:44:10 +0100

    model information added

--- application.rb
+++ application.rb
@@ -10,18 +10,21 @@ require 'sinatra/static_assets'
 use Rack::Flash
 set :sessions, true
 
-#class ToxPredictModel
-#	include DataMapper::Resource
-#	property :id, Serial
-#	property :name, String
-#	property :uri, String, :length => 255
-#	property :task_uri, String, :length => 255
-#	property :status, String, :length => 255
-#	property :messages, Text, :length => 2**32-1 
-#	property :created_at, DateTime
-#end
+class ToxCreateModel
+	include DataMapper::Resource
+	property :id, Serial
+	property :name, String
+	property :uri, String, :length => 255
+	property :task_uri, String, :length => 255
+	property :messages, Text, :length => 2**32-1 
+	property :created_at, DateTime
 
-#DataMapper.auto_upgrade!
+	def status
+		RestClient.get File.join(self.task_uri, 'status')
+	end
+end
+
+DataMapper.auto_upgrade!
 
 helpers do
 	def activity(a)
@@ -41,15 +44,23 @@ get '/?' do
 	redirect url_for('/create')
 end
 
-get '/model/:id/?' do
-	#@model = ToxPredictModel.get(params[:id])
-	@model = YAML.load(RestClient.get params[:uri], :accept => "application/x-yaml")
-	haml :model
+get '/models/?' do
+	@models = ToxCreateModel.all(:order => [ :created_at.desc ])
+	@refresh = true #if @models.collect{|m| m.status}.grep(/started|created/)
+	haml :models
+end
+
+get '/model/:id/delete/?' do
+	model = ToxCreateModel.get(params[:id])
+	RestClient.delete model.uri if model.uri
+	RestClient.delete model.task_uri if model.task_uri
+	model.destroy!
+	redirect url_for('/predict')
 end
 
 get '/predict/?' do 
-	#@models = ToxPredictModel.all
-	@models = OpenTox::Model::Lazar.find_all
+	@models = ToxCreateModel.all
+	@models = @models.collect{|m| m if m.status == 'completed'}.compact
 	haml :predict
 end
 
@@ -80,16 +91,8 @@ post '/upload' do # create a new model
 		flash[:notice] = "Please enter an endpoint name and upload a CSV file."
 		redirect url_for('/create')
 	end
-=begin
-	unless params[:file][:type] == "text/csv"
-		flash[:notice] = "Please upload a CSV file - at present we cannot handle other file types."
-		redirect url_for('/create')
-	end
-=end
-	#@model = ToxPredictModel.new
-	#@model.name = params[:endpoint]
-	#@model.status = "started"
-	#@model.save
+	@model = ToxCreateModel.new
+	@model.name = params[:endpoint]
 	title = URI.encode params[:endpoint]#.gsub(/\s+/,'_')
 	dataset = OpenTox::Dataset.new
 	dataset.title = title
@@ -130,28 +133,27 @@ post '/upload' do # create a new model
 	end
 	dataset_uri = dataset.save 
 	task_uri = OpenTox::Algorithm::Lazar.create_model(:dataset_uri => dataset_uri, :feature_uri => feature_uri)
+	@model.task_uri = task_uri
 
-	@notice = "Model creation for <b>#{params[:endpoint]}</b> (#{nr_compounds} compounds) started - this may take some time (up to several hours for large datasets). As soon as the model is ready it will appear in the list below, if you #{link_to("reload this page", "/predict")}."
+	@model.messages = "#{nr_compounds} compounds"
 
 	if smiles_errors.size > 0
-		@notice += "<p>The following Smiles structures were not readable and have been ignored:</p>"
-		@notice += smiles_errors.join("<br/>")
+		@model.messages += "<p>Incorrect Smiles structures (ignored):</p>"
+		@model.messages += smiles_errors.join("<br/>")
 	end
 	if activity_errors.size > 0
-		@notice += "<p>The following structures had irregular activities and have been ignored (please use 1 for active and 0 for inactive compounds):</p>"
-		@notice += activity_errors.join("<br/>")
+		@model.messages += "<p>Irregular activities (ignored - please use 1 for active and 0 for inactive compounds):</p>"
+		@model.messages += activity_errors.join("<br/>")
 	end
 	duplicate_warnings = ''
 	duplicates.each {|inchi,lines| duplicate_warnings += "<p>#{lines.join('<br/>')}</p>" if lines.size > 1 }
 	LOGGER.debug duplicate_warnings
 	unless duplicate_warnings == ''
-		@notice += "<p>The following structures were duplicated in the dataset (this is not a problem for the algorithm, but you should make sure, that the results were obtained from <em>independent</em> experiments):</p>" 
-		@notice +=  duplicate_warnings
+		@model.messages += "<p>Duplicated structures (all structures/activities used for model building, please  make sure, that the results were obtained from <em>independent</em> experiments):</p>" 
+		@model.messages +=  duplicate_warnings
 	end
-	session[:task_uri] = task_uri
-	#redirect url_for('/predict')
-	@models = OpenTox::Model::Lazar.find_all
-	haml :predict
+	@model.save
+	redirect url_for('/models')
 
 end
 
@@ -168,50 +170,35 @@ post '/predict/?' do # post chemical name to model
 		redirect url_for('/predict')
 	end
 	@predictions = []
-	params[:selection].keys.each do |uri|
+	LOGGER.debug params[:selection].to_yaml
+	params[:selection].keys.each do |id|
+		model = ToxCreateModel.get(id.to_i)
+		unless model.uri
+			model.uri = RestClient.get(File.join(model.task_uri, 'resource')).to_s
+			model.save
+		end
+		LOGGER.debug model.to_yaml
 		prediction = nil
 		confidence = nil
 		title = nil
 		db_activities = []
-		prediction = RestClient.post uri, :compound_uri => @compound.uri#, :accept => "application/x-yaml"
-		model = Redland::Model.new Redland::MemoryStore.new
+		prediction = RestClient.post model.uri, :compound_uri => @compound.uri#, :accept => "application/x-yaml"
+		redland_model = Redland::Model.new Redland::MemoryStore.new
 		parser = Redland::Parser.new
-		parser.parse_string_into_model(model,prediction,'/')
-		yaml = RestClient.get uri, :accept => 'application/x-yaml'
-		yaml = YAML.load yaml
-		title = URI.decode yaml[:endpoint].split(/#/).last
-		#f = model.subject(RDF['type'],OT['Feature']) # this can be dangerous if OWL is not properly sorted
-		#title = RestClient.get(File.join(uri,"name")).to_s
-		#title = model.object(f,DC['title']).to_s
-		model.subjects(RDF['type'], OT['FeatureValue']).each do |v|
-			feature = model.object(v,OT['feature'])
-			feature_name = model.object(feature,DC['title']).to_s
-			prediction = model.object(v,OT['value']).to_s if feature_name.match(/classification/)
-			confidence = model.object(v,OT['value']).to_s if feature_name.match(/confidence/)
-			db_activities << model.object(v,OT['value']).to_s if feature_name.match(/#{title}/)
+		parser.parse_string_into_model(redland_model,prediction,'/')
+		title = model.name
+		redland_model.subjects(RDF['type'], OT['FeatureValue']).each do |v|
+			feature = redland_model.object(v,OT['feature'])
+			feature_name = redland_model.object(feature,DC['title']).to_s
+			prediction = redland_model.object(v,OT['value']).to_s if feature_name.match(/classification/)
+			confidence = redland_model.object(v,OT['value']).to_s if feature_name.match(/confidence/)
+			db_activities << redland_model.object(v,OT['value']).to_s if feature_name.match(/#{title}/)
 		end
 		@predictions << {:title => title, :prediction => prediction, :confidence => confidence, :measured_activities => db_activities}
 	end
 
 		LOGGER.debug @predictions.to_yaml
 	haml :prediction
-	#@predictions.to_yaml 
-end
-
-post '/task/cancel' do
-	puts params[:task_uri]
-	task = OpenTox::Task.find(params[:task_uri])
-	task.cancel
-	redirect url_for('/tasks')
-end
-
-delete '/:id' do
-	#begin
-		OpenTox::Model::LazarClassification.delete(params[:id])
-		haml :index
-	#rescue
-		#"Deletion of model with ID #{params[:id]} failed. Please check your username and password."
-	#end
 end
 
 # SASS stylesheet
